f7a74a60ab62a0498565fe21707c0d0cca114754
Vivien Budavolgyi <budavolgyi.vivien@gmail.com>
2024-08-15 22:37:24 +0200

    add unit test for closelotteries function

--- src/test/scala/service/LotteryServiceTest.scala
+++ src/test/scala/service/LotteryServiceTest.scala
@@ -2,28 +2,34 @@ package service
 
 import cats.data.OptionT
 import cats.effect.IO
+import cats.effect.kernel.Ref
 import repository.{LotteryRepository, ParticipantRepository}
 import service.ServiceError.ValidationError
 import weaver.SimpleIOSuite
 import cats.implicits.*
 import domain.{Entry, EntryId, LotteryId, Participant, ParticipantId, Winner}
-import route.LotteryRoutes.EntryRequest
+import route.LotteryRoutes.{CloseResponse, EntryRequest, WinnerResponse}
 import service.LotteryService.invalidEntryErrorMessage
 
 import java.time.LocalDate
 
 object LotteryServiceTest extends SimpleIOSuite {
-  val entry        = 12
-  val entryRequest = EntryRequest(entry, 1)
+  val entryNumber  = 12
+  val entryRequest = EntryRequest(entryNumber, 1)
+  val entryId      = EntryId(entryNumber)
 
-  class TestLotteryRepository(submitEntryResult: IO[EntryId] = IO.pure(EntryId(entry))) extends LotteryRepository {
-    override def submitEntry(entry: Entry): IO[EntryId] = submitEntryResult
+  class TestLotteryRepository(
+    lotteryList: List[LotteryId] = Nil,
+    chooseWinnerResult: Option[Winner] = None,
+    savedWinners: Option[Ref[IO, List[Winner]]] = None,
+  ) extends LotteryRepository {
+    override def submitEntry(entry: Entry): IO[EntryId] = IO.pure(entryId)
 
-    override def getLotteries(): IO[List[LotteryId]] = ???
+    override def getLotteries(): IO[List[LotteryId]] = IO.pure(lotteryList)
 
-    override def chooseWinner(lotteryId: LotteryId, date: LocalDate): IO[Option[Winner]] = ???
+    override def chooseWinner(lotteryId: LotteryId, date: LocalDate): IO[Option[Winner]] = IO.pure(chooseWinnerResult)
 
-    override def saveWinner(winner: Winner, winDate: LocalDate): IO[Unit] = ???
+    override def saveWinner(winner: Winner, winDate: LocalDate): IO[Unit] = savedWinners.map(_.getAndUpdate(x => winner :: x)).sequence.as(())
   }
 
   class TestParticipantRepository(participantExistsResult: OptionT[IO, Unit] = OptionT.none, lotteryExistsResult: OptionT[IO, Unit] = OptionT.none)
@@ -53,5 +59,28 @@ object LotteryServiceTest extends SimpleIOSuite {
     for {
       result <- lotteryService.submitEntry(entryRequest)
     } yield expect(result == Left(ValidationError(invalidEntryErrorMessage)))
+  }
+
+  test("closeLotteries should save the chosen winners") {
+    val lottery1Number            = 1
+    val lotteryId1                = LotteryId(lottery1Number)
+    val winner                    = Winner(lotteryId1, entryId)
+    val testParticipantRepository = TestParticipantRepository()
+    for {
+      winnersRef           <- Ref[IO].of(Nil: List[Winner])
+      testLotteryRepository =
+        TestLotteryRepository(
+          lotteryList = List(lotteryId1),
+          chooseWinnerResult = Some(winner),
+          savedWinners = Some(winnersRef),
+        )
+      lotteryService        = LotteryService(testLotteryRepository, testParticipantRepository)
+      result               <- lotteryService.closeLotteries()
+      winnersResultList    <- winnersRef.get
+    } yield expect(
+      winnersResultList == List(winner) && result == CloseResponse(
+        List(WinnerResponse(lottery1Number, entryNumber)),
+      ),
+    )
   }
 }
